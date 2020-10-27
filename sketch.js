c0fc4c57f8f641f23bd885c453909f3094237236
unknown <raghavbhatia1004@gmail.com>
2020-10-27 17:25:20 +0530

    message

--- sketch.js
+++ sketch.js
@@ -40,6 +40,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    text("angrybirdgame",200,50)
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
